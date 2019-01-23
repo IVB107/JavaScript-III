269645f53bc26c932861f56cc26de6b520c1fe0b
IVB107 <ianvb107@gmail.com>
2019-01-23 15:19:42 -0600

    Completed Principle 2 in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window binding happens is when the "this" keyword is called inside the global scope. The window/DOM is the only context available that "this" can see.
-* 2. 
+* 2. Implicit binding is where "this" is used inside the context of an object method when that method is invoked. The context of "this" in implicit binding can be determined by looking to the left of the dot where the method is invoked.
 * 3. 
 * 4. 
 *
@@ -17,6 +17,13 @@ console.log(this.window);
 // Principle 2
 
 // code example for Implicit Binding
+const pet = {
+    name: 'Garfield',
+    likesTo: function(action){
+        console.log(`${this.name} likes to ${action}`);
+    }
+}
+console.log(pet.likesTo('eat lasagna.'));
 
 // Principle 3
 
