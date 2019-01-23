adec16e6ba2e4c3a7f831b30f1a28cbf886351b4
IVB107 <ianvb107@gmail.com>
2019-01-23 16:24:42 -0600

    Completed Principle 4 in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,13 @@
 *
 * 1. Window binding happens is when the "this" keyword is called inside the global scope. The window/DOM is the only context available that "this" can see.
 * 2. Implicit binding is where "this" is used inside the context of an object method when that method is invoked. The context of "this" in implicit binding can be determined by looking to the left of the dot where the method is invoked.
-* 3. For new binding, "this" points to the constructor object when it is created using the "new" keyword.
-* 4. 
+* 3. For new binding, with constructor functions, "this" points to the specific instance of the object that has been created (using the "new" keyword) and returned by the constructor function
+* 4. Explicit binding is used to attach properties from one constructor function to another using .call(), .apply(), or .bind() - the "this" keyword is explicitly defined as whatever object is used inside each of those three function methods.
+*
+*   - From MDN:
+*   - The call() method calls a function with a given this value and arguments provided individually.
+*   - The apply() method calls a function with a given this value, and arguments provided as an array (or an array-like object).
+*   - The bind() method creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
 *
 * write out a code example of each explanation above
 */
@@ -23,23 +28,45 @@ const pet = {
         console.log(`${this.name} likes to ${action}`);
     }
 }
-console.log(pet.likesTo('eat lasagna.'));
+pet.likesTo('eat lasagna.');
 
 // Principle 3
 
 // code example for New Binding
-function Car(sound){
-    this.make = 'Porsche';
-    this.model = '918 Spyder';
-    this.horsepower = 887;
-    this.sound = sound;
+function Vehicle(specs){
+    this.make = specs.make;
+    this.model = specs.model;
+    this.horsepower = specs.horsepower;
+    this.sound = specs.sound;
     this.purr = function(){
         return `A ${this.make} ${this.model} goes: "${this.sound}"`;
     }
 }
-const porsche = new Car('Vroom Vroom!');
+// Prototypical Inheritance:
+// Vehicle.prototype.purr = function(){
+//     return `A ${this.make} ${this.model} goes: "${this.sound}"`;
+// }
+
+const porsche = new Vehicle({
+    make: 'Porsche',
+    model: '918 Spyder',
+    horsepower: 887,
+    sound: 'Vroom Vroom!'
+});
 console.log(porsche.purr());
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const bigWheel = new Vehicle({
+    make: "Louis Marx & Co.",
+    model: "Big Wheel",
+    horsepower: '2 legs',
+    sound: "Look, ma!!"
+});
+
+bigWheel.purr.call(porsche);
+
+console.log(bigWheel.purr());
+
