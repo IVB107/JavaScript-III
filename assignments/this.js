effd3bfe6556757a7096416a9358c7a82177da66
IVB107 <ianvb107@gmail.com>
2019-01-23 15:05:17 -0600

    Completed Principle 1 in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. Window binding happens is when the "this" keyword is called inside the global scope. The window/DOM is the only context available that "this" can see.
 * 2. 
 * 3. 
 * 4. 
@@ -12,6 +12,7 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this.window);
 
 // Principle 2
 
