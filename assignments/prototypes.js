8a2fb4faaff2b14c05be46f826cf8f44ed9c9afc
IVB107 <ianvb107@gmail.com>
2019-01-24 14:50:07 -0600

    Refactored stretch code to use JavaScript classes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -75,79 +75,114 @@ class Humanoid extends CharacterStats{
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
 
-  // Hero
-function Hero(props){
-  Humanoid.call(this, props);
+// Hero
+
+class Hero extends Humanoid{
+  constructor(props){
+    super(props);
+  }
+  selectWeapon(){
+    let rand = Math.floor(Math.random() * this.weapons.length);
+    this.currentWeapon = this.weapons[rand];
+  }
+  attack(target){
+    this.selectWeapon();
+    if (this.currentWeapon === 'Combat Suit'){
+      this.healthPoints += 10;
+      this.combatSuitOn = true;
+      console.log(`${this.name} boosted up with his ${this.currentWeapon}`);
+      this.weapons.splice(3, 1);
+      this.status(target);
+      // Take another turn
+      this.attack(target);
+    }
+    console.log(`${this.name} attacked ${target.name} with his ${this.currentWeapon}`);
+    if (this.combatSuitOn){
+      target.healthPoints -= 7;
+    } else{
+      target.healthPoints -= 3;
+    }
+    if (this.healthPoints <= 5){
+      this.healthPoints += 5;
+    }
+    this.status(target);
+  }
+  revive(){
+    this.healthPoints += 5;
+    console.log(`${this.name} chugged a health potion.`);
+    this.status();
+  }
+  status(target){
+    console.log(`${this.name}'s HP: ${this.healthPoints}, ${target.name}'s HP: ${target.healthPoints}`);
+  }
 }
-
-Hero.prototype = Object.create(Humanoid.prototype);
 
 const rickSanchezC137 = new Hero({
   createdAt: new Date(),
@@ -169,44 +204,6 @@ const rickSanchezC137 = new Hero({
     currentWeapon: '',
     language: 'English',
 });
-
-Hero.prototype.selectWeapon = function(){
-  let rand = Math.floor(Math.random() * this.weapons.length);
-  this.currentWeapon = this.weapons[rand];
-}
-
-Hero.prototype.attack = function(target){
-  this.selectWeapon();
-  if (this.currentWeapon === 'Combat Suit'){
-    this.healthPoints += 10;
-    this.combatSuitOn = true;
-    console.log(`${this.name} boosted up with his ${this.currentWeapon}`);
-    this.weapons.splice(3, 1);
-    this.status(target);
-    // Take another turn
-    this.attack(target);
-  }
-  console.log(`${this.name} attacked ${target.name} with his ${this.currentWeapon}`);
-  if (this.combatSuitOn){
-    target.healthPoints -= 7;
-  } else{
-    target.healthPoints -= 3;
-  }
-  if (this.healthPoints <= 5){
-    this.healthPoints += 5;
-  }
-  this.status(target);
-}
-
-Hero.prototype.revive = function(){
-  this.healthPoints += 5;
-  console.log(`${this.name} chugged a health potion.`);
-  this.status();
-}
-
-Hero.prototype.status = function(target){
-  console.log(`${this.name}'s HP: ${this.healthPoints}, ${target.name}'s HP: ${target.healthPoints}`);
-}
 
 // Villain
 
